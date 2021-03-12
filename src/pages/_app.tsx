7664aa07b638b322b52b88f23a1e8a5969001957
Lucas Lopes <dev.thuram@gmail.com>
2021-03-12 09:20:08 -0300

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Xára</title>
+        <meta
+          name="description"
+          content="Pesquisa realizada pelo IBGE (nomes por década de nascimento)"
+        />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
